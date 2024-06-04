b1e7e28c6a64409b941b41f25270f3e54315916e
sanamghori <161497237+sanamghori@users.noreply.github.com>
2024-06-04 22:24:47 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,4 @@
+//Personal Message Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, //would you like to learn some Python today?”
+let firstName:string="Eric";
+let lastName:string="Hello"
+console.log(`${firstName}${lastName}`,"would you like to learn some Python today?")
